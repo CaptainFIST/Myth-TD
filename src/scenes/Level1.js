100879581349a6a33178b6c18871ce3594174a67
Edward Espinoza <edwespino@gmail.com>
2026-04-09 03:45:53 +0000

    Move all buttons from level 1 to GameUI

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -39,34 +39,18 @@ export default class Level1 extends Phaser.Scene {
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     ];
 
-    static tileTypes = { 
-        0: 'grass', 
-        1: 'path' 
+    static tileTypes = {
+        0: 'grass',
+        1: 'path'
     };
     static decoTypes = { 
-        2: 'tree' 
+        2: 'tree'
     };
 
     preload() {
         this.load.image('grass', 'assets/tiles/level1/grass_new.png');
         this.load.image('path', 'assets/tiles/level1/dirt_path.png');
         this.load.image('tree', 'assets/decorations/tree1.png');
-    }
-
-    createButton(x, y, label, onClick) {
-        const bg = this.add.rectangle(x, y, 160, 80, 0x000000)
-            .setOrigin(0).setStrokeStyle(2, 0xffffff)
-            .setInteractive({ useHandCursor: true }).setDepth(20);
-
-        const text = this.add.text(x + 80, y + 40, label, {
-            fontSize: '16px',
-            color: '#ffffff',
-            fontStyle: 'bold',
-            align: 'center'
-        }).setOrigin(0.5).setDepth(21);
-
-        bg.on('pointerdown', onClick);
-        return { bg, text };
     }
 
     create() {
@@ -76,24 +60,7 @@ export default class Level1 extends Phaser.Scene {
 
         this.scene.launch('UIManager', {
             player: this.player,
-            inventory: this.inventory,
-        });
-
-        const startY = this.scale.height - 80;
-        this.createButton(20, startY, 'Purchase Tower\n50 g', () => {
-            if (this.player.gold >= 50) {
-                this.player.updateGold(-50);
-
-                this.inventory.push({ type: 'unknown' });
-
-                this.scene.get('UIManager').updateInventoryUI();
-            }
-        });
-
-        const rightStartX = this.scale.width - 340;
-        this.createButton(rightStartX, startY, 'Merge', () => {});
-        this.createButton(rightStartX + 170, startY, 'Inventory', () => {
-            this.scene.get('UIManager').toggleInventory();
+            inventory: this.inventory
         });
     }
 }
