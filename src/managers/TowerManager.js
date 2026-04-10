518eaecc19e7fd35c34748f6e738dc86d77e8564
CaptainFIST <jr.barreda1017@gmail.com>
2026-04-10 06:16:37 -0500

    added tower manager partial

---
+++ src/managers/TowerManager.js
@@ -0,0 +1,31 @@
+export default class TowerManager {
+    constructor() {
+        // init tower data
+        this.neutral = [];
+
+        this.physical = [
+            ['Izanami', 15, 3, 0.7],
+            ['Susanoo', 10, 5, 3]
+        ];
+
+        this.air = [];
+
+        this.water = [];
+
+        this.fire = [];
+
+        this.dark = [];
+
+        this.other = [
+            ['Shrine', 2]
+        ];
+    }
+
+    tower() {
+
+    }
+
+    createTower() {
+
+    }
+}
